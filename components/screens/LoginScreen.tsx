5fa2746f3f731923bc36a818c3ca85824b54f53c
Julius Makel <julius.makel@gmail.com>
2023-06-04 00:30:07 +0200

    Add tailwind to the mix

--- components/screens/LoginScreen.tsx
+++ components/screens/LoginScreen.tsx
@@ -1,28 +1,38 @@
-import { NavigationProp, ParamListBase, RouteProp } from '@react-navigation/native';
+import {
+  NavigationProp,
+  ParamListBase,
+  RouteProp,
+} from '@react-navigation/native';
 import * as React from 'react';
-import { View, Text, Button, Linking } from "react-native";
+import {View, Text, Button, Linking, Pressable} from 'react-native';
 
 type LoginScreenProps = {
   route: RouteProp<ParamListBase, 'Login'>;
   navigation: NavigationProp<ParamListBase, 'Login'>;
 };
 
-function LoginScreen({ navigation }: LoginScreenProps) {
+function LoginScreen({navigation}: LoginScreenProps) {
   const authFlow = async () => {
     try {
-      await Linking.openURL("http://localhost:8080/auth/google/login");
+      await Linking.openURL('http://localhost:8080/auth/google/login');
     } catch (e) {
       console.error(e);
     }
-  }
+  };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Page</Text>
-      <Button title="Go to Tutorial" onPress={() => navigation.navigate('Tutorial')} />
-      <Button title="Sign in with Google" onPress={authFlow} />
+    <View className="flex items-center justify-center h-full">
+      {/*<View className="bg-slate-200 rounded-md">
+        <Button
+          title="Go to Tutorial"
+          onPress={() => navigation.navigate('Tutorial')}
+        />
+  </View>*/}
+      <View className="bg-slate-200 rounded-md">
+        <Button title="Sign in with Google" onPress={authFlow} />
+      </View>
     </View>
-  )
+  );
 }
 
 export default LoginScreen;
--- components/screens/UserScreen.tsx
+++ components/screens/UserScreen.tsx
@@ -1,6 +1,10 @@
-import { NavigationProp, ParamListBase, RouteProp } from '@react-navigation/native';
+import {
+  NavigationProp,
+  ParamListBase,
+  RouteProp,
+} from '@react-navigation/native';
 import * as React from 'react';
-import { View, Text, Button, Image } from "react-native";
+import { View, Text, Button, Image, TextInput } from 'react-native';
 import { AuthContext } from '../../App';
 
 type UserScreenProps = {
@@ -12,20 +16,50 @@ function UserScreen({ navigation }: UserScreenProps) {
   const { user, signOut } = React.useContext(AuthContext);
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>My Profile</Text>
-      {user &&
-        <>
-          <Image source={{ uri: user?.profile_picture_url }}
-            style={{ width: 400, height: 400 }} />
-          <Text>{user.name}</Text>
-          <Text>{user.age}</Text>
-          <Text>{user.locale}</Text>
-        </>
-      }
+    <View className="flex items-center">
+      <Image
+        className="m-2 bg-slate-400 rounded-full"
+        source={{ uri: user?.profile_picture_url }}
+        style={{ width: 100, height: 100 }}
+      />
+      <Text>{user?.name}</Text>
+      <Text>{user?.locale}</Text>
+      <View className="flex gap-2 mt-6 mb-4">
+        <Text>Age</Text>
+        <TextInput placeholder="Age" className="p-2 w-60 rounded-md border" />
+        <Text>Residence</Text>
+        <TextInput
+          placeholder="Residence"
+          className="p-2 w-60 rounded-md border"
+        />
+        <Text>Bio</Text>
+        <TextInput
+          placeholder="Bio"
+          className="max-h-20 h-full p-2 w-60 rounded-md border"
+          multiline
+        />
+        <Text>Languages</Text>
+        <TextInput
+          placeholder="Languages comma separated"
+          className="p-2 w-60 rounded-md border"
+          multiline
+        />
+        <Text>Interests</Text>
+        <TextInput
+          placeholder="Interests comma separated"
+          className="p-2 w-60 rounded-md border"
+          multiline
+        />
+        <Text>Countries</Text>
+        <TextInput
+          placeholder="Countries visited comma separated"
+          className="p-2 w-60 rounded-md border"
+          multiline
+        />
+      </View>
       <Button title="Sign out" onPress={signOut} />
     </View>
-  )
+  );
 }
 
 export default UserScreen;
